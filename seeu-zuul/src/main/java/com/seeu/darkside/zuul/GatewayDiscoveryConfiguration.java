8acf4a1c1da8f239c7e47f378bcfb2181aaa6881
thomasfouan <thomasfouan@hotmail.fr>
2018-03-31 13:48:01 +0200

    change config of zuul proxy

---
+++ seeu-zuul/src/main/java/com/seeu/darkside/zuul/GatewayDiscoveryConfiguration.java
@@ -0,0 +1,17 @@
+package com.seeu.darkside.zuul;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class GatewayDiscoveryConfiguration {
+
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient) {
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient);
+	}
+}
--- seeu-zuul/src/main/java/com/seeu/darkside/zuul/ZuulProxyApplication.java
+++ seeu-zuul/src/main/java/com/seeu/darkside/zuul/ZuulProxyApplication.java
@@ -2,11 +2,7 @@ package com.seeu.darkside.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulProxyApplication {
 
