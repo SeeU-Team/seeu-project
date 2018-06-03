9a3cf2d72eeb9814a1a3325c9616237e056112c1
thomasfouan <thomasfouan@hotmail.fr>
2018-06-03 21:02:33 +0200

    call method from user micro service to add user when it doesn't already exist in DB at login

--- seeu-gateway/src/main/java/com/seeu/darkside/gateway/user/User.java
+++ seeu-gateway/src/main/java/com/seeu/darkside/gateway/user/User.java
@@ -2,6 +2,8 @@ package com.seeu.darkside.gateway.user;
 
 import lombok.*;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -9,9 +11,23 @@ import lombok.*;
 @Setter
 public class User {
 
-	private Long idUser;
+	private Long id;
 
 	private Long facebookId;
 
-	private String name;
+	private String firstname;
+
+	private String lastname;
+
+	private String email;
+
+	private String password;
+
+	private String description;
+
+	private String profilePhotoUrl;
+
+	private Date created;
+
+	private Date updated;
 }
--- seeu-gateway/src/main/java/com/seeu/darkside/gateway/user/UserServiceProxy.java
+++ seeu-gateway/src/main/java/com/seeu/darkside/gateway/user/UserServiceProxy.java
@@ -2,6 +2,8 @@ package com.seeu.darkside.gateway.user;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("seeu-user-management")
@@ -9,4 +11,7 @@ public interface UserServiceProxy {
 
 	@GetMapping(value = "/users", params = "facebookId")
 	User getOneByFacebookId(@RequestParam(value="facebookId") Long id);
+
+	@PostMapping(value = "/users")
+	User createNewUser(@RequestBody User userDto);
 }
